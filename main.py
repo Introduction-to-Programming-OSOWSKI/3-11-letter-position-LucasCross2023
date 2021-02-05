68755c4639f755300e90fd9aca28be799f9dbd2c
LucasCross2023 <817750@mystma.org>
2021-02-05 00:16:56 +0000

    letter try 3

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def letterPos(w):
+def letterPos(w, x):
     count = 0
-    for i in range(len(w)):
-        if w[i] == "n":
-            count = i- 1
-    
-    return count 
+    for i in range(0,len(w)):
+        if w[i-1] == x:
+            count = i
+            return count 
+    else:
+        return "none"
 
-print(letterPos("sunshine"))
+print(letterPos("sunshine", "n"))
